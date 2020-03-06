2e75a3e22ab29a28a473ecb77e6b2456808401bb
Saaiheswar <saaiheswar@gmail.com>
2020-03-06 22:09:39 +0530

    First commit

---
+++ src/main/java/CustomizableCollections/App.java
@@ -0,0 +1,13 @@
+package CustomizableCollections;
+
+import java.util.List;
+public class App
+{
+    public static void main( String[] args ) {
+        List<Integer> l = new DemoList<Integer>(1,2,3,4,5,6,7,9,0,1);
+        l.add(10);
+        l.add(20);
+        System.out.println(l.get(1));
+        System.out.println(l);
+    }
+}
